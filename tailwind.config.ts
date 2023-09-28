093ac699d1ef83d062a34328f0d20d59e994539c
luonghoangkim <akashi192002@gmail.com>
2023-09-28 14:40:05 +0700

    v1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+  content: [ 
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
